d242f3a38771358d2fd32a2bf6c3cf11c2200d78
Manish <manishagrawal166@gmail.com>
2021-08-14 17:29:30 +0530

    Added Employee model

---
+++ src/EmployeeService/Models/Employees.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeService.Models
+{
+    public class Employees
+    {
+        public int Id { get; set; }
+        public string FName { get; set; }
+        public string LName { get; set; }
+        public string Department { get; set; }
+        public string JobTitle { get; set; }
+        public string Salary { get; set; }
+        public string Manager { get; set; }
+        public string Phone { get; set; }
+        public string DOB { get; set; }
+        public string Nationality { get; set; }
+        public string Email { get; set; }
+    }
+}
